b53c5874d573a0764bf199ef3837cb37be8e8e7f
knamost <sumansenpai@gmail.com>
2026-03-07 17:14:10 +0545

    Implement add button

--- gui_projects/To-Do/functions.py
+++ gui_projects/To-Do/functions.py
@@ -6,12 +6,11 @@ def get_todo(filepath=FILENAME):
         todos_local = file.readlines()
     return todos_local    
     
-
+    
 def write_todo(todos_arg, filepath=FILENAME ):
     """ Write the to-do items in list in the text file."""
     with open(filepath, 'w') as file:
         todo = file.writelines(todos_arg)
-
 
 if __name__ == "__main__":
     print("Hello")
