e5e04e66e0956f67990415d37588c0b99eda3333
knamost <sumansenpai@gmail.com>
2026-03-10 17:14:24 +0545

    python pdf template generator using fpdf

--- gui_projects/pdf-template/main.py
+++ gui_projects/pdf-template/main.py
@@ -2,16 +2,42 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation='P', unit='mm', format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 csv = pd.read_csv("topics.csv")
 
 for index, row in csv.iterrows():
     pdf.add_page()
     
+    # Set the header
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row['Topic'] , align="L", ln=1)
-    pdf.line(10, 21, 200, 21)
 
+    for y in range(20, 298, 10):
+        pdf.line(10, y, 200, y)
+        
+    # pdf.line(10, 21, 200, 21)   
+    
+    # Set the footer
+    pdf.ln(265)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=12, txt=row['Topic'], align="R")
+
+
+    for i in range(row['Pages'] - 1):
+        pdf.add_page()
+        # Set the footer
+        pdf.ln(277)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=12, txt=row['Topic'], align="R")
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
+        
+    # pdf.line(10, 21, 200, 21)   
+        
+        
 
 pdf.output('output.pdf')
